7dd75e2e262acac1e6815c8260c7832ef24a8852
Alex <Alexiosdut@gmail.com>
2019-09-03 03:21:03 +0300

    changed gallery image quality to 70

--- src/templates/collectionGallery.js
+++ src/templates/collectionGallery.js
@@ -17,7 +17,7 @@ export const query = graphql`
       title
       description
       gallery {
-        fluid(maxWidth: 1800, quality: 80) {
+        fluid(maxWidth: 1800, quality: 70) {
           ...GatsbyContentfulFluid_withWebp
         }
       }
