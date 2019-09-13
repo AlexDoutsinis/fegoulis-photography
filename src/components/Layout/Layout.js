4553175ad6be561885dc940688004cc990e56bf7
Alex <Alexiosdut@gmail.com>
2019-09-13 16:44:46 +0300

    add global styles to layout component instead of index page

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "../../globalStyle.css"
 
 import Header from "./Header"
 import Footer from "./Footer"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 import React from "react"
-import SEO from "../components/SEO"
 
+import SEO from "../components/SEO"
 import Layout from "../components/Layout/Layout"
 import Collections from "../components/Collections/Collections"
-
-import "../globalStyle.css"
 
 const IndexPage = () => (
   <Layout>
