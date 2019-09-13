25ffb7e064ae95fbf2fad2054ed5d25114f6d6d5
Alex <Alexiosdut@gmail.com>
2019-09-13 06:41:55 +0300

    edit thanks page

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,10 +1,36 @@
 import React from "react"
+import styled from "styled-components"
+import media from "styled-media-query"
 
 import Layout from "../components/Layout/Layout"
 
+const Wrapper = styled.div`
+  width: 60vw;
+  height: 70vh;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  ${media.lessThan("medium")`
+    width: 100vw;
+  `}
+`
+
+const H2 = styled.h2`
+  font-size: 1.2rem;
+
+  ${media.lessThan("small")`
+    width: 80vw;
+    font-size: 1rem;
+  `}
+`
+
 const Thanks = () => (
   <Layout>
-    <h2>Thanks for your message, I will contact you as soon as possible.</h2>
+    <Wrapper>
+      <H2>Thanks for your message, I will be in contact soon.</H2>
+    </Wrapper>
   </Layout>
 )
 
